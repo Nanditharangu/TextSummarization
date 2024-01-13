ed05df058fa3aba723e9bf37666a24a73eb0bf5f
Nanditharangu <nanditharangu2806@gmail.com>
2024-01-13 17:50:17 +0530

    notebook added

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]:%(message)s:')
 project_name = "textSummarizer"
 
 list_of_files = [
-    ".github/workflow/.gitkeep",
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
